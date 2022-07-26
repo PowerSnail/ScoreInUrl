f943cab5c10e1e19b9c7595ee7c39fd95ff56531
PowerSnail <hj@powersnail.com>
2022-07-26 04:09:15 -0700

    use codemirror

--- main.js
+++ main.js
@@ -5,47 +5,40 @@ import 'abcjs/abcjs-audio.css';
 import { processScore } from './src/music.js'
 import { decode_score, encode_score } from './src/url'
 import { default_score } from './src/default_score'
+import { basicSetup } from 'codemirror';
+import { EditorView } from "@codemirror/view"
 
-var editor = document.getElementById("editor")
 var label_url = document.getElementById("url")
 var label_warn = document.getElementById("warnings")
-var btn_copy = document.getElementById("copy_url")
+var btn_copy = document.getElementById("copyUrl")
 
-function onScoreChange() {
-    var score = editor.value
-    label_url.value = encode_score(score)
-    var visualObj = processScore(score)
-    if (visualObj[0].warnings == undefined) {
-        label_warn.innerHTML = ""
-    } else {
-        label_warn.innerHTML = visualObj[0].warnings
-    }
-}
-
-function onBtnCopyClicked() {
+btn_copy.addEventListener("click", _ => {
     if (window.isSecureContext) {
         navigator.clipboard.writeText(label_url.value);
     } else {
         alert("This page is not viewed in a secure context, so copy is unuseable. Please click the url and press ctrl-C to copy.")
     }
-}
-
-editor.addEventListener("input", onScoreChange)
-btn_copy.addEventListener("click", onBtnCopyClicked)
+})
 label_url.addEventListener("focus", _ => label_url.setSelectionRange(0, label_url.value.length))
 
-function init() {
-    const score = decode_score()
-    if (score != null) {
-        editor.value = score
-        onScoreChange()
-    } else if (window.location.search != "") {
-        alert("Invalid URL, going back to Home")
-        window.location.assign(window.location.origin)
-    } else {
-        editor.value = default_score
-        onScoreChange()
-    }
+let score = decode_score()
+if (score == null && window.location.search != "") {
+    alert("Invalid URL, going back to Home")
+    window.location.assign(window.location.origin)
 }
+score = score || default_score
 
-init()
+let editor = new EditorView({
+    extensions: [
+        basicSetup,
+        EditorView.updateListener.of(function (e) {
+            let score = e.state.doc.toString()
+            let visualObj = processScore(score)
+            label_warn.innerHTML = visualObj[0].warnings || ""
+            label_url.value = encode_score(score)
+        }),
+    ],
+    parent: document.getElementById("editorWrapper")
+})
+editor.dispatch(editor.state.update({ changes: { from: 0, to: 0, insert: score } }))
+document.body.classList.remove("invisible")
